e97e7a8c3a40a2ea148acc26e15aa1e62439ec3b
snortuser <101020136+snortuser@users.noreply.github.com>
2022-03-23 23:58:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>Oh geez... What reality am I in?</title>
 </head>
 
-<!-- TODO: Remember to upgrade Azure subscription before trial ends. Company credit card is 1234-5678-1234-5678. -->
+<!-- TODO: Remember to upgrade Azure subscription before trial ends. -->
   
 <body>
 <h1> Server Information</h1>
